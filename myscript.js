b7ea9edda9d68bee42215da828fce9fb4fcc8a2d
Julia Testa Matos <jutemts@gmail.com>
2025-05-20 08:52:21 -0300

    atualizado

--- myscript.js
+++ myscript.js
@@ -1,10 +1,10 @@
-let toggle = document.getElementById('toggle')                       //localiza no html o botao com id toggle
+let mudar = document.getElementById('mudar')                       //localiza no html o botao com id toggle
 let themeSystem = localStorage.getItem('themeSystem') || 'dark'      //verifica se ja existe um tema salvo no navegador.
 
 mudarTema(themeSystem); //aplica o tema atual que esta no localstorage
 
-if(toggle){ //verifica se o botao existe
-    toggle.addEventListener('click', () => {  //add determinada acao quando eu 'clicar'. quando clicar vai acontecer oq   tem dentro dessa chave.
+if(mudar){ //verifica se o botao existe
+    mudar.addEventListener('click', () => {  //add determinada acao quando eu 'clicar'. quando clicar vai acontecer oq   tem dentro dessa chave.
     let temaAtual =document.documentElement.getAttribute('data-theme'); //le o valor atual que esta em data-theme
 
     if(temaAtual === 'dark'){
@@ -16,11 +16,11 @@ if(toggle){ //verifica se o botao existe
     
    localStorage.setItem('themeSystem',novoTema); //salva o novo tema no localstorage 
    
-   mudarTema(novoTema) //aplica novo tema
-    })
+   mudarTema(novoTema) //aplica novo tema que foi guardado anteriomente
+    }) 
 }   
 //essa parte meio que serve para mudar no css a paleta de uma pra outra 
-   function mudarTema(theme){                                     //o parametro theme pode ser light ou dark
+   function mudarTema(theme){ //theme vai receber o valor que foi passado anteriormente no novo tema. 
     document.documentElement.setAttribute('data-theme',theme);      //se o theme for light, substitui as cores do dark 
 }
 
