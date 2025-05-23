fa95db5b9b06cb9f8c1393454b52dcc79567c59f
JuliaTesta <jutemts@gmail.com>
2025-05-23 11:36:10 -0300

    atualizado

--- myscript.js
+++ myscript.js
@@ -1,11 +1,11 @@
-let mudar = document.getElementById('mudar')                       //localiza no html o botao com id toggle
+let mudar = document.getElementById('mudar')                       //localiza no html o botao com id mudar
 let themeSystem = localStorage.getItem('themeSystem') || 'dark'      //verifica se ja existe um tema salvo no navegador.
 
-mudarTema(themeSystem); //aplica o tema atual que esta no localstorage
+mudarTema(themeSystem);                                                //aplica o tema atual que esta no localstorage
 
-if(mudar){ //verifica se o botao existe
-    mudar.addEventListener('click', () => {  //add determinada acao quando eu 'clicar'. quando clicar vai acontecer oq   tem dentro dessa chave.
-    let temaAtual =document.documentElement.getAttribute('data-theme'); //le o valor atual que esta em data-theme
+if(mudar){                                                                       //verifica se o botao existe
+    mudar.addEventListener('click', () => { 
+    let temaAtual =document.documentElement.getAttribute('data-theme');           //le o valor atual que esta em data-theme
 
     if(temaAtual === 'dark'){
         novoTema ='light'
@@ -14,13 +14,13 @@ if(mudar){ //verifica se o botao existe
         novoTema='dark'
     }
     
-   localStorage.setItem('themeSystem',novoTema); //salva o novo tema no localstorage 
+   localStorage.setItem('themeSystem',novoTema);                                    //salva o novo tema no localstorage 
    
-   mudarTema(novoTema) //aplica novo tema que foi guardado anteriomente
+   mudarTema(novoTema)                                                    //aplica novo tema que foi guardado anteriomente
     }) 
 }   
-//essa parte meio que serve para mudar no css a paleta de uma pra outra 
-   function mudarTema(theme){ //theme vai receber o valor que foi passado anteriormente no novo tema. 
-    document.documentElement.setAttribute('data-theme',theme);      //se o theme for light, substitui as cores do dark 
+
+   function mudarTema(theme){  
+    document.documentElement.setAttribute('data-theme',theme); 
 }
 
